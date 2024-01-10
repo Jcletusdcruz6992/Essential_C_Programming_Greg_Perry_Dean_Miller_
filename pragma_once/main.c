453a355e576f6250b4de8d3bb150b3c3744357ef
James Cletus Dcruz <72412570+Jcletusdcruz6992@users.noreply.github.com>
2024-01-10 18:41:23 -0500

    adding

---
+++ pragma_once/main.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include"math.h"
+#include"print.h"
+int main(){
+printFunc(add(17,9));
+return 0;
+}
---
+++ pragma_once/print.h
@@ -0,0 +1,6 @@
+#pragma once
+#include<stdio.h>
+
+void printFunc(int num){
+printf("Sum=%d \n");
+}
